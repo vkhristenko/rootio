66d3ff06b9a0961ddb04a6a8c65d20733015149f
vkhristenko <vdkhristenko1991@gmail.com>
2018-09-05 22:24:34 +0200

    adding explicit preconditions and postconditions for read/write records operations

--- iolayer.c
+++ iolayer.c
@@ -20,7 +20,7 @@ void read_streamer_record(struct llio_t* llio) {
 
     // deser key and assign the blob
     from_buf_key(&buffer, &llio->streamer_record.key);
-    llio->streamer_record.blob = (void*)tmp;
+    llio->streamer_record.blob = (char*)buffer;
 }
 
 void read_free_segments_record(struct llio_t* llio) {
@@ -73,7 +73,7 @@ struct llio_t open_to_read(char *filename) {
 struct llio_t open_to_write(char *filename) {
     struct llio_t llio;
     llio.fctx = open_context(filename, "wb");
-    llio.top_dir_record.key.obj_name.str = filename;
+    llio.top_dir_rec.key.obj_name.str = filename;
 
     // write the "root" header
     char *root = "root";
@@ -93,56 +93,58 @@ void close_from_write(struct llio_t *llio) {
 
 void write_file_header(struct llio_t* llio) {
     fseek(llio->fctx.pfile, 4, SEEK_SET);
-    llio->header.begin = 100;
-    llio->header.units = 4;
-    llio->header.compress = 1;
     
-    char tmp[300];
+    char tmp[100];
     char *buffer = tmp;
     to_buf_file_header(&buffer, &llio->header);
-    root_write(llio.fctx, tmp, llio->header.begin);
-    llio->location = llio->header.begin;
+    root_write(llio->fctx, tmp, llio->header.begin - 4);
 }
 
 void write_top_dir_record(struct llio_t *llio) {
-     llio->top_dir_record.key.version = 4;
-     llio->top_dir_record.key.date_time.raw = 1234;
-     llio->top_dir_record.key.cycle = 1;
-     llio->top_dir_record.key.seek_key = header.begin;
-     llio->top_dir_record.key.seek_pdir = 0;
-     llio->top_dir_record.key.class_name.str = "TFile";
-     llio->top_dir_record.key.class_name.size = 5;
-     llio->top_dir_record.key.obj_name.str = filename;
-     /*
-             52     key.obj_name.size = strlen(filename);
-              53     key.obj_title.size = 0;
-               54     key.key_bytes = size_key(&key);
-                55 
-                     56     named.name.str = key.obj_name.str;
-                 57     named.name.size = key.obj_name.size;
-                  58     named.title.str = key.obj_title.str;
-                   59     named.title.size = key.obj_title.size;
-                    60 
-                         61     dir.version = 5;
-                     62     dir.date_time_c.raw = 1234;
-                      63     dir.date_time_m.raw = 1235;
-                       64     dir.nbytes_name = size_key(&key) + size_named(&named);
-                        65     dir.seek_dir = header.begin;
-                         66     dir.seek_parent = 0;
-                          67 
-                               68     // to get the size of bytes for the dir, we need to know the version first
-                                69     key.obj_bytes = size_dir(&dir);
-                           70     key.total_bytes = key.key_bytes + key.obj_bytes + size_named(&named);
+    fseek(llio->fctx.pfile llio->header.begin, SEEK_SET);
 
-                            73     char *buf_toprec = malloc(key.total_bytes);
-                             74     char *buf_toprec_to_free = buf_toprec;
-                              75     to_buf_key(&buf_toprec, &key);
-                               76     to_buf_named(&buf_toprec, &named);
-                                77     to_buf_dir(&buf_toprec, &dir);
-                                 78     root_write(ctx, buf_toprec_to_free, key.total_bytes);
-                                  79 
-                                       80     next_location_to_write += key.total_bytes;
-                                   81 
-                                        82     dir.seek_keys = next_location_to_write;
-                                        */
+    char *buffer = malloc(llio->top_dir_rec.key.total_bytes);
+    char *tmp = buffer;
+    to_buf_key(&buffer, &llio->top_dir_rec.key);
+    to_buf_key(&buffer, &llio->top_dir_rec.named);
+    to_buf_dir(&buffer, &llio->top_dir_rec.dir);
+    root_write(llio->fctx, tmp, llio->top_dir_rec.key.total_bytes);
+
+    free(tmp);
+}
+
+void write_streamer_record(struct llio_t *llio) {
+    fseek(llio->fctx.pfile, llio->location, SEEK_SET);
+    
+    // postcondition
+    llio->streamer_record.key.seek_key = llio->location;
+
+    char *buffer = malloc(llio->streamer_record.key.key_bytes);
+    char *tmp = buffer;
+    to_buf_key(&buffer, llio->streamer_record.key);
+    root_write(llio->fctx, tmp, llio->streamer_record.key.key_bytes);
+    root_write(llio->fctx, llio->streamer_record.key.blob, 
+               llio->streamer_record.key.total_bytes - llio->streamer_record.key.key_bytes);
+
+    // postcondition
+    llio->location += llio->streamer_record.key.total_bytes;
+    free(tmp);
+}
+
+void write_free_segments_record(struct llio_t *llio) {
+    fseek(llio->fctx.pfile, llio->location, SEEK_SET);
+
+    // postcondition
+    llio->free_segments_record.key.seek_key = llio->location;
+
+    char *buffer = malloc(llio->free_segments_record.key.total_bytes);
+    char *tmp = buffer;
+    to_buf_key(&buffer, &llio->free_segments_record.key);
+    for (int i=0; i<llio->free_segments_record.length; i++)
+        to_buf_pfree(&buffer, &(llio->free_segments_record.pfree[i]));
+    root_write(llio->fctx, tmp, llio->free_segments_record.key.total_bytes);
+
+    // postcondition
+    llio->location += llio->free_segments_record.key.total_bytes;
+    free(tmp);
 }
--- iolayer.h
+++ iolayer.h
@@ -9,11 +9,22 @@ struct top_dir_record_t {
     struct PDirectory dir;
 };
 
+/**
+ * Streamer Record. For now a simple impl
+ *   Key + TList of StreamerElements
+ */
 struct streamer_record_t {
     struct PKey key;
-    void *blob;
+    // blob points to the location where TList of Streamers starts
+    // to free the acquired space, move blob pointer back by key_bytes: blob -= key.key_bytes and then free
+    char *blob;
 };
 
+/**
+ * Free Segmnets Record. For now a simple array impl.
+ * TODO: does this come into play when we have more than single write/read multiple times, but also allow
+ *   to modify the objects on disk -> for instance root allows to delete a record from disk
+ */
 struct free_segments_record_t {
     struct PKey key;
     int length;
@@ -21,10 +32,15 @@ struct free_segments_record_t {
 };
 
 struct llio_t {
+    // owns
     uint64_t location;
+    // owns
     struct FileContext fctx;
+    // owns
     struct PFileHeader header;
+    // owns
     struct top_dir_record_t top_dir_rec;
+    // 
     struct streamer_record_t streamer_record;
     struct free_segments_record_t free_segments_record;
 };
@@ -45,8 +61,39 @@ void read_streamer_record(struct llio_t*);
 void read_free_segments_record(struct llio_t*);
 void read_top_dir_record(struct llio_t*);
 
+/**
+ * Writing logic step by step:
+ * 1. write header
+ * 2. write top directory record (given a name, we know the full size)
+ * 3 -> N. Write Records
+ * N+1. Write Top Dir Kyes List Record
+ * N+2. ReWrite Top Directory Record
+ * N+3. Write Streamer Record
+ * N+4. Write Free Segment Record
+ * N+5. Write End Byte
+ * N+6. ReWrite flle header
+ * 
+ * Preconditions before writing a record:
+ *  1. key and object blob are already properly generated
+ *  2. key only misses the location to itself (position on file where it will be written)
+ *
+ * Postconditions after a record is written
+ *  1. record is modified - key is updated to reflect the position in the file to write it to.
+ *    Imporatnt for records that  depend on records that are written before them
+ *  2. llio struct is modified to reflect the next position to write to in the file
+ *  3. all the resources allocated within the function call are released
+ *
+ * Postconditions after a special record is written (streamer or free segments records):
+ *  - for streamer -> modify the header struct to reflect the location of the streamer record in file
+ *  - for free segments -> the same 
+ */
+
+// file header and top dir record do not increment the location
+// as they are written twice, at open and second time at close
 void write_file_header(struct llio_t*);
 void write_top_dir_record(struct llio_t*);
+
+// do increment the location counter
 void write_streamer_record(struct llio_t*);
 void write_free_segments_record(struct llio_t*);
 void write_end_byte(struct llio_t*);
