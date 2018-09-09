0942b7c343f9df24385ae09229998fa6ac459769
vkhristenko <vdkhristenko1991@gmail.com>
2018-09-09 18:45:01 +0200

    adding python tests

---
+++ test_python/test_read.py
@@ -0,0 +1,14 @@
+import ctypes
+import sys
+
+from sugar import *
+from defs import *
+
+if __name__ == "__main__":
+    context = """
+    test_read functionality
+    """
+    print context
+    filename = sys.argv[1]
+    path2lib = sys.argv[2]
+
---
+++ test_python/test_read_sequentially.py
@@ -0,0 +1,10 @@
+import ctypes
+
+from sugar import *
+from defs import *
+
+if __name__ == "__main__":
+    context = """
+    test_read sequentially functionality
+    """
+    print context
